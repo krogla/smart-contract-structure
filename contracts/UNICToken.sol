5fed5766009fab8dde84401a0ff73e2b5e976f18
UNIC Advertising Network <35422932+unicads@users.noreply.github.com>
2018-01-14 23:55:55 +0200

    Update UNICToken.sol

--- contracts/UNICToken.sol
+++ contracts/UNICToken.sol
@@ -48,11 +48,18 @@ contract owned {
 
 
 contract TokenERC20{
-    
-    
-    
-    
-    
+
+    function totalSupply() constant external returns (uint256) {
+        return _supply;
+    }
+    function balanceOf(address owner) public constant returns (uint256 balance);
+    function transfer(address to, uint256 value) public returns (bool success);
+    function transferFrom(address from, address to, uint256 value) public returns (bool success);
+    function approve(address spender, uint256 value) public returns (bool success);
+    function allowance(address owner, address spender) public constant returns (uint256 remaining);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 
@@ -61,26 +68,12 @@ contract UNICToken is owned, TokenERC20 {
     
     using SafeMath for uint;
     
-    string public constant name = 'UNIC';
+    string public constant name = 'UNICToken';
     string public constant symbol = 'UNIC';
     uint8 public constant decimals = 18;
+    
+    uint256 public totalSupply = 250000000 * 10 ** uint256(decimals);
     
     
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
