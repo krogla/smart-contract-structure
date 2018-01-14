0131b5cdcec4292a36246e4a3093ecfdbb0e076a
UNIC Advertising Network <35422932+unicads@users.noreply.github.com>
2018-01-14 15:44:46 +0200

    Create UNICToken.sol

---
+++ contracts/UNICToken.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.4.18;
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract owned {
+    address public owner;
+
+    function owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+}
+
+
+
+contract TokenERC20{
+    
+    
+    
+    
+    
+}
+
+
+
+contract UNICToken is owned, TokenERC20, SafeMath {
+    
+    string public constant name = 'UNIC';
+    string public constant symbol = 'UNIC';
+    uint8 public constant decimals = 18;
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
